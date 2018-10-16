e524830345f4e95b15e1563752c464c740519a87
bil.iman.fickry@gmail.com <fickry@Fickrys-Macbook-Pro.local>
2018-10-16 12:00:25 +0700

    exercise while dan exercise for, done

---
+++ exerciseFor.js
@@ -0,0 +1,15 @@
+console.log('===============');
+console.log('LOOPING PERTAMA');
+console.log('===============');
+
+for (let i = 1; i <= 20; i++) {
+    console.log(`${i} - I Love Coding`);
+}
+
+console.log('===============');
+console.log('LOOPING KEDUA');
+console.log('===============');
+
+for (let i = 20; i > 0; i--) {
+    console.log(`${i} - I Will Become Fullstack Developer`);
+}
---
+++ exerciseWhile.js
@@ -0,0 +1,20 @@
+let loop1 = 0;
+let loop2 = 20;
+
+console.log('===============');
+console.log('LOOPING PERTAMA');
+console.log('===============');
+
+while (loop1 < 20) {
+    loop1 += 2;
+    console.log(`${loop1} - I Love Coding`);
+}
+
+console.log('===============');
+console.log('LOOPING KEDUA');
+console.log('===============');
+
+while (loop2 > 0) {
+    console.log(`${loop2} - I Will Become Fullstack Developer`);
+    loop2 -= 2;
+}
