ca98814ed69e1da56222918ab6e5816390ba0072
bil.iman.fickry@gmail.com <fickry@Fickrys-Macbook-Pro.local>
2018-10-16 13:40:30 +0700

    exercise function, done

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 4. Exercise Melakukan Perulangan Menggunakan while.
 5. Exercise Melakukan Perulangan Menggunakan for.
 6. Exercise Mampu menggabungkan konsep Conditional dan Looping dalam kasus sederhana.
+7. Exercise Function.
--- exerciseFunction.js
+++ exerciseFunction.js
@@ -20,5 +20,13 @@ console.log(hasilPerkalian);
 
 // Tugas 3
 function processSentence(a, b, c, d) {
-    return `Nama saya ${a}, umur saya ${b}, alamat saya di ${c}, dan saya punya hobby yaitu ${d}`;
+    return `Nama saya ${a}, umur saya ${b}, alamat saya di ${c}, dan saya punya hobby yaitu ${d}!`;
 }
+
+let name = "Agus";
+let age = 30;
+let address = "Jln. Malioboro, Yogjakarta";
+let hobby = "gaming";
+
+let fullSentence = processSentence(name, age, address, hobby);
+console.log(fullSentence);
